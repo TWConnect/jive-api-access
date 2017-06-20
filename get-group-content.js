ffc7ba7bb21e92c3eb24c36e494450b4bab92254
htao <htao@thoughtworks.com>
2017-06-20 18:17:41 +0800

    [taohui] add lambda function to test http request

---
+++ get-group-content.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+
+exports.handler = function(event, context) {
+  console.log('start request to ' + event.url)
+  http.get(event.url, function(res) {
+    console.log("Got response: " + res.statusCode);
+    context.succeed("hello");
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+    context.done(null, 'FAILURE');
+  });
+
+  console.log('end request to ' + event.url);
+}
