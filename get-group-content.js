20df4e5808163b847f932069a64e81d8e1b4d956
Shengzhi Wang <szwang@thoughtworks.com>
2017-06-21 17:12:57 +0800

    [shengzhi]#338 get contents by group id after search

--- get-group-content.js
+++ get-group-content.js
@@ -1,8 +1,27 @@
 var request = require('request');
+var auth = '';
+
+function errorHandler(error, response, context){
+  if (error !== null || response.statusCode !== 200) {
+    console.log('error:', error);
+    context.done(null, 'FAILURE');
+  }
+}
+
+function getContents(result, context) {
+  request({
+    uri: 'https://thoughtworks-preview.jiveon.com/api/core/v3/places/' + result.list[0].placeID + '/contents',
+    headers: {
+      'Authorization': auth
+    }
+  }, function (error, response, body) {
+    errorHandler(error, response, context);
+    context.succeed(JSON.parse(body));
+  });
+}
 
 exports.handler = function(event, context) {
   var searchGroup = 'https://thoughtworks-preview.jiveon.com/api/core/v3/search/places?filter=search(' + event.groupName+ ')';
-  var auth = '';
   var options = {
     uri: searchGroup,
     headers: {
@@ -11,12 +30,14 @@ exports.handler = function(event, context) {
   };
 
   request(options, function (error, response, body) {
-    if (error !== null || response.statusCode !== 200) {
-      console.log('error:', error);
-      context.done(null, 'FAILURE');
-    }
+    errorHandler(error, response, context);
     var result = JSON.parse(body);
-    context.succeed(result.list.length);
+    if (result.list.length > 1) {
+      context.done(result.list, 'There are many groups');
+    } else {
+      console.log('place id', result.list[0].placeID);
+      getContents(result, context);
+    }
     console.log('statusCode:', response && response.statusCode);
 
   });
